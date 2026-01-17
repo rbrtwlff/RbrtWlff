d98d1b0ae9b43ef3cf29d7427e9b1500c96b49ae
rbrtwlff <rbrtwlff@aol.com>
2026-01-17 20:41:04 +0100

    Fix WPF type ambiguities

--- src/AkteTimer/App.xaml.cs
+++ src/AkteTimer/App.xaml.cs
@@ -4,7 +4,7 @@ using AkteTimer.Views;
 
 namespace AkteTimer;
 
-public partial class App : Application
+public partial class App : System.Windows.Application
 {
     private TrayService? _trayService;
     private HotkeyService? _hotkeyService;
